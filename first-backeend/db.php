a32708a67258a49500828ee3eb63ddd371d4c20a
yacine <yacinekun17@gmail.com>
2026-03-29 21:49:02 +0100

    Remove db.php from repo (security fix)

--- first-backeend/db.php
+++ first-backeend/db.php
@@ -1,8 +1,8 @@
 <?php
-$host ='localhost';
-$username ='root';
+$host ='';
+$username ='';
 $password ='';
-$DB_name ='Db_ToDoList';
+$DB_name ='';
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $conn = new mysqli($host,$username,$password,$DB_name);
 if($conn->connect_error){
