fc36fd30515294773b44f023ff4e9321478cd290
YACINE HAFAYED <yacinehafayed3@gmail.com>
2026-03-31 10:00:51 +0100

    db.php

--- first-backeend/db.php
+++ first-backeend/db.php
@@ -6,6 +6,6 @@ $DB_name ='';
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $conn = new mysqli($host,$username,$password,$DB_name);
 if($conn->connect_error){
-die ('connection failed');
+die ('connection failed'); 
 }
 ?> 
